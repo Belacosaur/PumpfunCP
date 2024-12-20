11030645581b78bf6ccb0bc3a921a52468d4011e
Caleb Martin <calebjmartin@hotmail.com>
2024-12-20 21:45:26 +0700

    build errors

---
+++ next.config.mjs
@@ -0,0 +1,29 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      fs: false,
+      net: false,
+      tls: false,
+      crypto: false,
+      stream: false,
+      http: false,
+      https: false,
+      zlib: false,
+      path: false,
+      os: false,
+    };
+    return config;
+  },
+  transpilePackages: [
+    "@solana/wallet-adapter-base",
+    "@solana/wallet-adapter-react",
+    "@solana/wallet-adapter-react-ui",
+    "@solana/wallet-adapter-wallets",
+    "@solana/web3.js",
+  ],
+};
+
+export default nextConfig; 
